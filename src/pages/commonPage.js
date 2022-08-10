3cbd51d28e72c119f91a4cfd3dd8598f6bda414c
punipuni21 <punihobby@gmail.com>
2022-08-10 23:12:34 +0900

    create commonPage.js

---
+++ src/pages/commonPage.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import commonStyle from "../const/commonStyle";
+import AppBar from "@material-ui/core/AppBar";
+import { Toolbar } from "@material-ui/core";
+import Typography from "@material-ui/core";
+import { MultiThemeProvider } from "@material-ui/core";
+
+class CommonPage extends Component {
+  render() {
+    return (
+      <MultiThemeProvider>
+        <AppBar>
+          <Toolbar>
+            <Typography>CANDY CHUPS PAatisserie</Typography>
+          </Toolbar>
+        </AppBar>
+      </MultiThemeProvider>
+    );
+  }
+}
