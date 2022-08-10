c9f9bb9854e68cfdd835bd842a20458db752a950
punipuni21 <punihobby@gmail.com>
2022-08-11 00:12:03 +0900

    add cake list

---
+++ src/const/cakeList.js
@@ -0,0 +1,16 @@
+export const CakeListSetting = {
+  initialList: [
+    { name: "ショートケーキ", price: 350, stock: 10 },
+    { name: "チーズケーキ", price: 380, stock: 8 },
+    { name: "シュークリーム", price: 250, stock: 15 },
+    { name: "ロールケーキ", price: 250, stock: 5 },
+    { name: "モンブラン", price: 450, stock: 6 },
+  ],
+  tableSetting: [
+    { label: "名前", data: "name" },
+    { label: "値段", data: "price" },
+    { label: "在庫数", data: "stock" },
+    { label: "売る", data: "sell" },
+    { label: "在庫数を増やす", data: "refill" },
+  ],
+};
