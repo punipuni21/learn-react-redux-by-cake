0e810019f0657329915ab5b65d4ea7e12f2d8b1c
punipuni21 <punihobby@gmail.com>
2022-08-10 23:41:52 +0900

    create commonStyle.js

---
+++ src/const/commonStyle.js
@@ -0,0 +1,18 @@
+import { createTheme } from "@material-ui/core";
+import pink from "@material-ui/core/colors/pink";
+import amber from "@material-ui/core/colors/amber";
+
+const commonStyle = () => {
+  createTheme({
+    palette: {
+      primary: {
+        main: pink[300],
+      },
+      secondary: {
+        main: amber[400],
+      },
+    },
+  });
+};
+
+export default commonStyle;
