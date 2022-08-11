a969f9e94f52314f958de895a2a9b40fae54e9f1
punipuni21 <punihobby@gmail.com>
2022-08-11 14:49:09 +0900

    add action type

--- src/action/actionTypes.js
+++ src/action/actionTypes.js
@@ -1,6 +1,7 @@
 //initialize
 export const SET_CAKE_LIST = "SET_CAKE_LIST";
+export const SET_MATERIAL_STOCK = "SET_MATERIAL_STOCK";
 
-export const SELL_SHORTCAKE = "SELL_SHORTCAKE";
+export const SELL_CAKE = "SELL_CAKE";
 
 // module.exports = { SET_CAKE_LIST, SELL_SHORTCAKE };
