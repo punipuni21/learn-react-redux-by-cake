58a3866c9500101e6cf153b883b2569dd85b5539
punipuni21 <punihobby@gmail.com>
2022-08-11 15:30:15 +0900

    add action

--- src/action/actionTypes.js
+++ src/action/actionTypes.js
@@ -4,6 +4,7 @@ export const SET_MATERIAL_STOCK = "SET_MATERIAL_STOCK";
 
 export const SELL_CAKE = "SELL_CAKE";
 export const MAKE_CAKE = "MAKE_CAKE";
+export const BUY_MAKERIAL = "BUY_MATERIAL";
 
 export const CONSUME_MATERIAL = "CONSUME_MATERIAL";
 export const REFILL_MATERIAL = "REFILL_MATERIAL";
