ac477a73bb0c68022077e50c1693474888c7fc43
punipuni21 <punihobby@gmail.com>
2022-08-11 13:49:17 +0900

    add state

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import CommonPage from "./pages/commonPage";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import cakeListReducer from "./reducer/cakeListReducer";
+
+let store = configureStore(cakeListReducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <CommonPage></CommonPage>
-  </React.StrictMode>
+  <Provider store={store}>
+    <CommonPage />
+  </Provider>
 );
