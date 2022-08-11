f2b389cd52fb9832ba10906b623037ab67a113c6
punipuni21 <punihobby@gmail.com>
2022-08-11 14:01:34 +0900

    configureStoreに変更

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import "./index.css";
 import CommonPage from "./pages/commonPage";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-import cakeListReducer from "./reducer/cakeListReducer";
+import cakeListReducer from "./Reducer/cakeListReducer";
 
-let store = configureStore(cakeListReducer);
+let store = configureStore({
+  reducer: cakeListReducer,
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
